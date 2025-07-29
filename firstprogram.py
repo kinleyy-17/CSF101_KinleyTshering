a45c6f2b34c67a798484d019b38e2222eb62f84d
kinleyy-17 <Jigmjy17@gmail.com>
2025-07-29 14:50:08 +0600

    change

---
+++ firstprogram.py
@@ -0,0 +1,9 @@
+print("Hello SWE")
+print("Hello CST")
+a=7
+b=8
+sum=a+b
+print(sum)
+name=input("Enter your name")
+age=input("Enter your age")
+print("Your Naame",name, "and your are",age, "years old")
